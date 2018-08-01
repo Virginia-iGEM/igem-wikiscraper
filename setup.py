84d0ea6b554157b0ffc00a2bf51d0db31edec423
Mantissa-23 <dylanculfogienis@gmail.com>
2018-07-31 23:36:54 -0400

    Preparing for PyPi publishing

--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name = 'igemwikiscraper',
-    version = '0.4.0',
+    version = '0.4.1',
     description = 'GUI+CLI+Library webscraper built specifically for iGEM team wiki pages',
     url = 'https://github.com/Virginia-iGEM/igem-wikiscraper',
     author='Dylan Culfogienis',
     author_email='dtc9bb@virginia.edu',
     license='MIT',
     packages=['igemwikiscraper'],
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent'
+    ),
     install_requires = [
         'requests', 
         'beautifulsoup4',
         'lxml',
-        'gooey'
+        'gooey>=1.0.1'
     ],
     entry_points = {
         'console_scripts': [
